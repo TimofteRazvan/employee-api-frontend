c71d5b26f7eaa34a58d5f10cadb37bc224d45d8e
917_Timofte_Razvan <92137529+TimofteRazvan@users.noreply.github.com>
2023-04-03 10:56:00 +0300

    HELP

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,7 +12,8 @@ export default function Home() {
     }, []);
 
     const loadEmployees = async () => {
-        const result = await axios.get("http://localhost:8080/employees-details")
+        const result = await axios.get("api/employees-details")
+        //const result = await axios.get("http://localhost:8080/employees-details")
         setEmployees(result.data);
     };
 
