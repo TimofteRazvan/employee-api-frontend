994ade9f17aa6e0801f59d921171b5a6e310d22b
917_Timofte_Razvan <92137529+TimofteRazvan@users.noreply.github.com>
2023-04-03 10:05:14 +0300

    api help cors

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,7 +12,7 @@ export default function Home() {
     }, []);
 
     const loadEmployees = async () => {
-        const result = await axios.get("http://api/employees-details")
+        const result = await axios.get("api/employees-details")
         //const result = await axios.get("http://3.76.207.9:80/employees-details")
         //const result = await axios.get("http://localhost:8080/employees-details")
         setEmployees(result.data);
