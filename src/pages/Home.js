ed09d864a547d430f622e8f3b3bac9a6eafb5a7b
917_Timofte_Razvan <92137529+TimofteRazvan@users.noreply.github.com>
2023-04-03 09:33:25 +0300

    commit cors

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,13 +12,13 @@ export default function Home() {
     }, []);
 
     const loadEmployees = async () => {
-        const result = await axios.get("http://3.76.207.9:80/employees-details")
+        const result = await axios.get("https://cors-anywhere.herokuapp.com/http://3.76.207.9:80/employees-details")
         //const result = await axios.get("http://localhost:8080/employees-details")
         setEmployees(result.data);
     };
 
     const deleteEmployee = async (id) => {
-        await axios.delete(`http://3.76.207.9:80/employees/${id}`)
+        await axios.delete(`https://cors-anywhere.herokuapp.com/http://3.76.207.9:80/employees/${id}`)
         //await axios.delete(`http://localhost:8080/employees/${id}`)
         loadEmployees();
     };
