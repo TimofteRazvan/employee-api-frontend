9330d8bbbc19bbb95498e4e1a093cfd62abb6b9a
917_Timofte_Razvan <92137529+TimofteRazvan@users.noreply.github.com>
2023-05-12 20:06:30 +0300

    Checking stuff #1

--- src/addresses/Addresses.js
+++ src/addresses/Addresses.js
@@ -14,6 +14,7 @@ export default function Addresses() {
     const loadAddresses = async () => {
         const result = await axios.get(`api/addresses/page/${i}/${nr}`)
         //const result = await axios.get(`http://localhost:8080/addresses/page/${i}/${nr}`)
+        console.log(result.data);
         setAddresses(result.data);
     };
 
